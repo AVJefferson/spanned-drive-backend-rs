362f13285a089b950009dc02408ebeccd9105616
A V Jefferson <avjefferson@bajajauto.co.in>
2026-04-15 10:32:12 +0000

    changed google-web to google-drive

--- src/routes/token/mod.rs
+++ src/routes/token/mod.rs
@@ -6,5 +6,5 @@ use axum::Router;
 use std::sync::Arc;
 
 pub fn routes(app_state: Arc<AppState>) -> Router {
-    Router::new().nest("/google-web", google_web::routes(app_state.clone()))
+    Router::new().nest("/google-drive", google_web::routes(app_state.clone()))
 }
